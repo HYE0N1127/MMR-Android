2cc221c6c02164e8e7b639fba031232634055e41
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-07-13 23:54:24 +0900

    ADD : SharedPreference를 이용한 버튼 Enabled 이벤트 추가

--- app/src/main/java/kr/hs/dgsw/mmr/view/fragment/ProfileFragment.kt
+++ app/src/main/java/kr/hs/dgsw/mmr/view/fragment/ProfileFragment.kt
@@ -1,18 +1,23 @@
 package kr.hs.dgsw.mmr.view.fragment
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import kr.hs.dgsw.mmr.R
 import kr.hs.dgsw.mmr.base.BaseFragment
 import kr.hs.dgsw.mmr.databinding.FragmentProfileBinding
 import kr.hs.dgsw.mmr.factory.NoParameterViewModelFactory
+import kr.hs.dgsw.mmr.view.activity.ManagePostActivity
+import kr.hs.dgsw.mmr.view.activity.StoreMaterialActivity
 import kr.hs.dgsw.mmr.viewmodel.fragment.ProfileViewModel
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>() {
@@ -31,7 +36,27 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>()
     override fun observerViewModel() {
         with(mViewModel) {
             changeNameEvent.observe(this@ProfileFragment, {
+                Log.d("MYTAG", "click : ${name.value}")
 
+
+            })
+            storeMaterialEvent.observe(this@ProfileFragment, {
+                val intent = Intent(activity, StoreMaterialActivity::class.java)
+                startActivity(intent)
+            })
+            managePostEvent.observe(this@ProfileFragment, {
+                val intent = Intent(activity, ManagePostActivity::class.java)
+                startActivity(intent)
+            })
+
+            name.observe(this@ProfileFragment, Observer {
+                if(!hasNamed.value!!) {
+                    Log.d("MYTAG", "false")
+                    hasNamed.value = true
+                } else {
+                    Log.d("MYTAG", "Change Name : ${name.value}")
+                    canClick.value = true
+                }
             })
         }
     }
--- app/src/main/java/kr/hs/dgsw/mmr/viewmodel/fragment/ProfileViewModel.kt
+++ app/src/main/java/kr/hs/dgsw/mmr/viewmodel/fragment/ProfileViewModel.kt
@@ -8,9 +8,12 @@ import kr.hs.dgsw.mmr.utils.SingleLiveEvent
 class ProfileViewModel : BaseViewModel() {
 
     val name = MutableLiveData<String>()
+    // Shared Preference에 사용되는 name값
+
+    val hasNamed = MutableLiveData<Boolean>(false)
+    val canClick = MutableLiveData<Boolean>(false)
 
     val changeNameEvent = SingleLiveEvent<Unit>()
-    val defaultName = MutableLiveData<String>()
 
     val storeMaterialEvent = SingleLiveEvent<Unit>()
     val managePostEvent = SingleLiveEvent<Unit>()
